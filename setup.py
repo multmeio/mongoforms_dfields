d1c4481bc2ed11d1fea15c526697eef185b5fea9
Luis Araujo <luis@multmeio.com.br>
2012-08-30 17:31:31 -0300

    configure setup installation for pip

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
-setup.py
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='mongoforms_dfields',
+    version='0.',
+    description='A ModelForm-DynamicFields',
+    author=u'Luís Araújo',
+    author_email='luis@multmeio.com.br',
+    maintainer=u'Luís Araújo',
+    maintainer_email='luis@multmeio.com.br',
+    url='https://github.com/multmeio/mongoforms_dfields',
+    packages=find_packages(
+        exclude=['examples', 'examples.*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    requires=['Django', 
+	    'mongoengine(>=0.6)', 
+	    'pymongo(>=2.1)', 
+	    'django_mongoforms(>=0.3)']
+)
